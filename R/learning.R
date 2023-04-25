bc80853a8e9897c373083c421b59b8906564e489
Jo Frøkjær Iversen <jo.iversen@sund.ku.dk>
2023-04-25 15:45:22 +0200

    Important message regarding commitments!

--- R/learning.R
+++ R/learning.R
@@ -17,4 +17,6 @@ mean(2:6)
 library(tidyverse)
 
 # This will be used for testing out Git! <3 You're the best, never seen a better coder than you!
+# Just a very important commitment thing: Just because you've committed the file within Git, does NOT mean you've committed it to Github - this still needs to be done later.
+# I'm just saying this, because I was a dumb dumb and wrote in my first commitments that I was adding them to GitHub, and I definitely was not. Just committing them to Git.
 
