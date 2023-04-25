18774d2b98afc0eef4169178c12ed61f0163cf2c
Jo Frøkjær Iversen <jo.iversen@sund.ku.dk>
2023-04-25 15:39:48 +0200

    Adding the /R files to Github

---
+++ R/learning.R
@@ -0,0 +1,17 @@
+# Basics of R -------------------------------------------------------------
+
+colnames(airquality)
+
+str(airquality)
+
+summary(airquality)
+
+mean(2:6)
+
+# Ctrl-shift-p and then search for "style active file" - which will clean up
+# your entire file.
+
+
+# Loading Packages --------------------------------------------------------
+
+library(tidyverse)
